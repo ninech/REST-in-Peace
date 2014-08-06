ca82b3d4e905c9e3ddc7efc75e92c9305eeb34b4
Raffael Schmid <raf@nine.ch>
2014-08-06 16:27:56 +0200

    fix faraday namespace

--- lib/rest_in_peace/faraday/raise_errors_middleware.rb
+++ lib/rest_in_peace/faraday/raise_errors_middleware.rb
@@ -8,15 +8,15 @@ module RESTinPeace
       def on_complete(env)
         case env[:status]
         when 404
-          raise Faraday::Error::ResourceNotFound, response_values(env)
+          raise ::Faraday::Error::ResourceNotFound, response_values(env)
         when 407
           # mimic the behavior that we get with proxy requests with HTTPS
-          raise Faraday::Error::ConnectionFailed, %{407 "Proxy Authentication Required "}
+          raise ::Faraday::Error::ConnectionFailed, %{407 "Proxy Authentication Required "}
         when 422
           # do not raise an error as 422 from a rails app means validation errors
           # and response body contains the validation errors
         when CLIENT_ERROR_STATUSES
-          raise Faraday::Error::ClientError, response_values(env)
+          raise ::Faraday::Error::ClientError, response_values(env)
         end
       end
 
