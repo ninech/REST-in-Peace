cf5bb88ac3a69ecfb9d6ccf1eea1c3f9d1c8722b
Raffael Schmid <raf@nine.ch>
2014-08-06 11:35:12 +0200

    :nail_care: for :dog:

--- lib/rest_in_peace/faraday/raise_errors_middleware.rb
+++ lib/rest_in_peace/faraday/raise_errors_middleware.rb
@@ -3,7 +3,7 @@ require 'faraday'
 module RESTinPeace
   module Faraday
     class RaiseErrorsMiddleware < ::Faraday::Response::Middleware
-      ClientErrorStatuses = 400...600
+      CLIENT_ERROR_STATUSES = 400...600
 
       def on_complete(env)
         case env[:status]
@@ -15,13 +15,17 @@ module RESTinPeace
         when 422
           # do not raise an error as 422 from a rails app means validation errors
           # and response body contains the validation errors
-        when ClientErrorStatuses
+        when CLIENT_ERROR_STATUSES
           raise Faraday::Error::ClientError, response_values(env)
         end
       end
 
       def response_values(env)
-        {:status => env.status, :headers => env.response_headers, :body => env.body}
+        {
+          status: env.status,
+          headers: env.response_headers,
+          body: env.body,
+        }
       end
     end
 
